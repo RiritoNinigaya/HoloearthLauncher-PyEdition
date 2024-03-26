833cef5e97c287fad984c676cdb75078fc3b96e0
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-03-26 16:41:44 +0200

    Update download_holoearth.py

--- HoloearthLauncher-PythonEdition/download_holoearth.py
+++ HoloearthLauncher-PythonEdition/download_holoearth.py
@@ -2,7 +2,8 @@ import requests
 import urllib.request
 from CONSTANTS import CONST
 import mp3play
+import os
 class DownloadHolo:
     def Download():
-        req_download = CONST.download_holoearth_str
-        urllib.request.urlretrieve(req_download, "Holoearth.zip")
+        req_download = str(CONST.download_holoearth_str)
+        urllib.request.urlretrieve(req_download, "{}".format(os.getcwd() + "\\Holoearth.zip"))
